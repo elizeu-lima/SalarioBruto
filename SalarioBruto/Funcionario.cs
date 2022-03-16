901e4ebf9db87fa1be57389b04b2cc9b9efcef36
elizeu-lima <elizeucoca@gmail.com>
2022-03-15 21:02:55 -0300

    Adicionar arquivos de projeto.

---
+++ SalarioBruto/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioBruto{
+    class Funcionario{
+        public string? Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido(){
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem){
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+        public override string ToString(){
+           return Nome
+           + " , Salário Liquido: R$ "
+           + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SalarioBruto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioBruto{
+    class Program{
+        static void Main(string[] args){
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Entre com os dados do funcionário: ");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("--------------------------------------------------------------");
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
